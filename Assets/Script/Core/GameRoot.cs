0f9e7cf89497ebce7ff03219f3899e2fb6182007
강유진 <kang.yj099@gamil.com>
2026-04-15 13:04:15 +0900

    리팩토링: InputManager를 GameRoot로 옮김

--- Assets/Script/Core/GameRoot.cs
+++ Assets/Script/Core/GameRoot.cs
@@ -8,9 +8,12 @@ public sealed class GameRoot : SingletonMono<GameRoot>
     public SceneLoadManager SceneLoadManager { get; private set; }
     public GameStateManager GameStateManager { get; private set; }
 
+    public InputActionManager InputActionManager { get; private set; }
+
+
     protected override void OnAwake()
     {
-        InitManagers(); //매니저 컴포넌트
+        InitManagers(); //매니저 초기화
     }
 
     private void Start()
@@ -25,15 +28,22 @@ public sealed class GameRoot : SingletonMono<GameRoot>
 
     protected override void OnDestroyed()
     {
+        InputActionManager?.Release();
     }
 
     private void InitManagers()
     {
+        // 컴포넌트 캐싱
         SceneLoadManager = GetComponentInChildren<SceneLoadManager>();
         GameStateManager = GetComponentInChildren<GameStateManager>();
 
         // 매니저가 없을 경우 에러 메시지 출력
         if (SceneLoadManager == null) Debug.LogError("SceneLoadManager를 찾을 수 없습니다!");
         if (GameStateManager == null) Debug.LogError("GameStateManager를 찾을 수 없습니다!");
+
+
+        // 스크립트 초기화
+        InputActionManager = new InputActionManager();
+        InputActionManager.Init();
     }
 }
--- Assets/Script/SceneManager/GameSceneManager.cs
+++ Assets/Script/SceneManager/GameSceneManager.cs
@@ -4,15 +4,10 @@ public class GameSceneManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
 
-    private InputActionManager inputActionManager;
-
     PlayerController playerInstance;
 
     private void Awake()
     {
-        inputActionManager = new InputActionManager();
-        inputActionManager.Init();
-
         if (playerPrefab == null)
         {
             Debug.LogError("Player Object is not assigned in the inspector.\n인스펙터에 플레이어 오브젝트를 등록해주세요!");
@@ -27,7 +22,6 @@ public class GameSceneManager : MonoBehaviour
 
     private void OnDestroy()
     {
-        inputActionManager?.Release();
     }
 
     private void SpawnPlayer()
@@ -36,6 +30,6 @@ public class GameSceneManager : MonoBehaviour
         player.transform.position = Vector3.zero; // 초기 위치 설정
 
         playerInstance = player.GetComponent<PlayerController>();
-        playerInstance.Init(inputActionManager);
+        playerInstance.Init(GameRoot.Instance.InputActionManager);
     }
 }

c793eb41ced79d6102b5041bcbef10258ab77798
강유진 <kang.yj099@gamil.com>
2026-04-21 03:00:37 +0900

    기능: 씬에 종속되는 Singleton 클래스 추가

--- Assets/Script/Core/Singleton.cs
+++ Assets/Script/Core/Singleton.cs
@@ -29,3 +29,27 @@ public class SingletonMonoDontDestroy<T> : MonoBehaviour where T : Component
     protected virtual void OnAwake() { }
     protected virtual void OnDestroyed() { }
 }
+
+public class SingletonMono<T> : MonoBehaviour where T : Component
+{
+    public static T Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            return;
+        }
+        Instance = this as T;
+        OnAwake();
+    }
+    protected virtual void OnDestroy()
+    {
+        OnDestroyed();
+        if (Instance == this)
+            Instance = null;
+        Debug.Log($"SingletonMono<{typeof(T).Name}> 인스턴스 파괴.");
+    }
+    protected virtual void OnAwake() { }
+    protected virtual void OnDestroyed() { }
+}

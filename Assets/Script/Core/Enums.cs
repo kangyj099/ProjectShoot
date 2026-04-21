6b5e2ea20debe21981cbf98c3ee96f70efe73a44
강유진 <kang.yj099@gamil.com>
2026-04-22 02:51:39 +0900

    기능: Enum 스크립트 추가

---
+++ Assets/Script/Core/Enums.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public enum ObjectType
+{
+    None = -1,
+    Player,     // Actor
+    Monster,    // Actor
+    Projectile,
+    Item,
+    Claw,
+
+    Count // enum 개수 세는 용도, 실제로는 사용하지 않음
+}
+
+public enum ItemType
+{
+
+}
+
+public enum CollisionType
+{
+
+}

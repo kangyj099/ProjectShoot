c27db153131de240d9a2af4ac68d36bceb7cf7a1
강유진 <kang.yj099@gamil.com>
2026-04-21 21:24:09 +0900

    리팩토링: 풀 매니저의 풀 딕셔너리 키 Data기준에서 Prefab기준으로 변경, 디버그 로그 수정

--- Assets/Script/Core/ObjectPool/PoolManager.cs
+++ Assets/Script/Core/ObjectPool/PoolManager.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    [SerializeField] private Dictionary<ObjectData, IPool> pools = new();
+    [SerializeField] private Dictionary<GameObject, IPool> pools = new();
 
     public T Get<T>(ObjectData data) where T : IPoolable
     {
         var poolable = data.Prefab as IPoolable;
         if (poolable == null)
         {
-            Debug.LogError($"풀에서 생성할 오브젝트는 반드시 IPoolable 컴포넌트를 가지고있어야 함.\n{data.Prefab.gameObject.name}의 {data.Prefab.name}은 IPoolable 컴포넌트가 아님");
-            return default;
+            if (TryGetComponent(out IPoolable poolableComponent))
+            {
+                poolable = poolableComponent;
+                Debug.LogWarning($"풀에서 생성할 오브젝트의 컴포넌트는 BaseObject와 IPoolable 컴포넌트를 같이 상속해야 함.\n{data.Prefab.gameObject.name}의 {data.Prefab.name}은 IPoolable 컴포넌트가 아님");
+            }
+            else
+            {
+                Debug.LogError($"풀에서 생성할 오브젝트는 반드시 IPoolable 컴포넌트를 가지고있어야 함.\n{data.Prefab.gameObject.name}의 {data.Prefab.name}은 IPoolable 컴포넌트가 아님");
+                return default;
+            }
         }
 
         // 풀 없으면 새로운 풀 생성
-        if (!pools.TryGetValue(data, out var pool))
+        if (!pools.TryGetValue(data.Prefab.gameObject, out var pool))
         {
             // 풀 객체 모아둘 그룹 만들기
             GameObject poolParent = new(data.Prefab.gameObject.name + " Pool");
@@ -28,17 +36,17 @@ public class PoolManager : MonoBehaviour
             var poolType = typeof(Pool<>).MakeGenericType(componentType);
             // 동적으로 생성한 타입으로 객체 생성
             pool = (IPool)Activator.CreateInstance(poolType, data.Prefab, poolParent.transform, 64, 1024);
-            pools[data] = pool;
+            pools[data.Prefab.gameObject] = pool;
         }
 
         return (T)pool.Get();
     }
 
-    public bool Release<T>(T item) where T : Component, IPoolable
+    public bool Release<T>(T item) where T : IPoolable
     {
         if (item.Pool == null)
         {
-            Debug.LogError($"풀에 반환할 오브젝트는 반드시 IPoolable 컴포넌트를 가지고있어야 함.\n{item.name}은 IPoolable 컴포넌트가 아님");
+            Debug.LogError($"반환할 풀이 없음.");
             return false;
         }
 
@@ -51,7 +59,7 @@ public class PoolManager : MonoBehaviour
         var poolable = obj.GetComponent<IPoolable>();
         if (poolable == null)
         {
-            Debug.LogError($"풀에 반환할 오브젝트는 반드시 IPoolable 컴포넌트를 가지고있어야 함.\n{obj.name}은 IPoolable 컴포넌트가 아님");
+            Debug.LogError($"풀에 반환할 오브젝트는 반드시 IPoolable 컴포넌트를 가지고있어야 함.\n{obj.name}은 IPoolable 컴포넌트가 없음");
             return false;
         }
 
@@ -68,12 +76,12 @@ public class PoolManager : MonoBehaviour
         pools.Clear();
     }
 
-    public void Dispose(ObjectData prefab)
+    public void Dispose(ObjectData data)
     {
-        if (pools.TryGetValue(prefab, out var pool))
+        if (pools.TryGetValue(data.Prefab.gameObject, out var pool))
         {
             pool.Clear();
-            pools.Remove(prefab);
+            pools.Remove(data.Prefab.gameObject);
         }
     }
 }

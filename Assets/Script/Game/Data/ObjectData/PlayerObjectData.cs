4890be8bc4a8ef963af3513aea2218a7cb557937
강유진 <kang.yj099@gamil.com>
2026-04-26 02:27:08 +0900

    파일: 스크립터블오브젝트 생성 우클릭 메뉴 수정

--- Assets/Script/Game/Data/ObjectData/PlayerObjectData.cs
+++ Assets/Script/Game/Data/ObjectData/PlayerObjectData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerObjectData", menuName = "Scriptable Objects/ObjectData/PlayerObjectData")]
+[CreateAssetMenu(fileName = "PlayerObjectData", menuName = "Scriptable Object/Object Data/Player Object Data")]
 public class PlayerObjectData : ObjectData
 {
     public override void Initialize(BaseObject instance)
--- Assets/Script/Test/TestObjectData.cs
+++ Assets/Script/Test/TestObjectData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// <fileName>생성되는 에셋 파일 기본 이름</fileName>
 /// <menuName>프로젝트 우클릭 메뉴 Create 카테고리 어느부분에서 어떤 이름으로 찾을 수 있는지</menuName>
 /// </summary>
-[CreateAssetMenu(fileName = "TestObjectData", menuName = "ScriptableObjects/Test/TestObjectData", order = 1)]
+[CreateAssetMenu(fileName = "TestObjectData", menuName = "Scriptable Object/Test/TestObjectData", order = 1)]
 public class TestObjectData : ObjectData
 {
     [SerializeField] private Sprite sprite;

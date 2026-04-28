ed45e8d526d2b7c986d7e94098d21d426a935f54
강유진 <kang.yj099@gamil.com>
2026-04-28 21:35:04 +0900

    버그수정: PlayerObject스크립트 에러 수정
    커밋 누락사항 업로드

--- Assets/Script/Game/Object/ActorObject.cs
+++ Assets/Script/Game/Object/ActorObject.cs
@@ -3,4 +3,13 @@ using UnityEngine;
 public abstract class ActorObject : BaseObject
 {
     public override bool IsActor => true;    // ActorObject 자식 클래스에서 true로 오버라이드
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+    }
+
+    protected override void InitCollisionEntity()
+    {
+    }
 }

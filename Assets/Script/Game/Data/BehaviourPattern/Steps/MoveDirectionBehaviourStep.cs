875d12191e9a2778755e3324c69b828423c21b2b
강유진 <kang.yj099@gmail.com>
2026-07-19 02:56:59 +0900

    기능: 행동패턴 MoveDirection 방향성 이동 추가

---
+++ Assets/Script/Game/Data/BehaviourPattern/Steps/MoveDirectionBehaviourStep.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MoveDirectionBehaviourStep :BehaviourStep
+{
+    public override string Name => "방향 이동";
+    public override BehaviourType Type => BehaviourType.MoveDirection;
+    // 이동시간
+    public float Duration { get; set; }
+    // 이동 방향
+    public Vector2 Direction { get; set; }
+
+    public override void Start(BehaviourPatternRunner runner) { }
+
+    public void Begin(BehaviourPatternRunner runner)
+    {
+        runner.SetDirection(Direction);
+    }
+    protected override void OnExecute(BehaviourPatternRunner runner)
+    {
+
+    }
+    public override bool CheckStepComplete(BehaviourPatternRunner runner)
+    {
+        return runner.StepElapsedTime >= Duration;
+    }
+
+    public override void Stop(BehaviourPatternRunner runner)
+    {
+        runner.SetDirection(Vector2.zero);
+    }
+}
